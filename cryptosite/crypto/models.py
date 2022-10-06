a553bea69a14a82ccf79aa6e31b2147609e255ff
fofiktel <093456jy@gmail.com>
2022-10-06 16:10:12 +0300

    Add project to git

---
+++ cryptosite/crypto/models.py
@@ -0,0 +1,16 @@
+
+from django.db import models
+# Create your models here.
+
+
+class CryptoList(models.Model):
+    CurrencyFullName = models.CharField(max_length=40)
+    CurrencyAbbreviatedName = models.CharField(max_length=10)
+    CostInUsd = models.FloatField(default=0)
+    MarketCap = models.IntegerField(default=0)
+    CirculatingSupply = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.CurrencyFullName
+
+# Create your models here.
